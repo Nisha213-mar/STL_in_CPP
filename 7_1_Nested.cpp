d9bbb0091bcc9f920b866a11d421846c35ed26cc
Nisha Jha <83752180+Nisha213-mar@users.noreply.github.com>
2022-06-15 21:26:33 +0530

    ques1_Nested

---
+++ 7_1_Nested.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    map<int,multiset<string>>marks_map;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;++i){
+        int marks;
+        string name;
+        cin>>name>>marks;
+        marks_map[marks].insert(name);
+    }
+    auto cur_it=--marks_map.end();
+    while(true){
+        auto &students = (*cur_it).second;
+        int marks =(*cur_it).first;
+        for(auto student: students){
+            cout<<student<<" "<<marks<<endl;
+        }
+        if(cur_it==marks_map.begin()) break;
+        cur_it--;
+    }
+}
